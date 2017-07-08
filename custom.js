4ccaa08a75b9710cc57537295f7824175fbb6dee
Abhishek <parmarabhishek.53@gmail.com>
2017-07-08 12:30:11 +0530

    music player layout completed

---
+++ custom.js
@@ -0,0 +1,48 @@
+// function to add song
+$('#submit_song').click(function(){
+    var song_name = $('#song_to_upload').val().replace(/C:\\fakepath\\/i, '');
+    $("audio").attr("src","songs/" + song_name);
+});
+// work for paying the song on click of button
+$('.play_btn').click(function(){
+    arr = $('.song_play');
+    if(arr[0].paused == true)
+        {
+            arr[0].play();
+            $('#icon_toggle').removeClass('fa-play');
+            $('#icon_toggle').addClass('fa-pause');
+            $('body').addClass('add_gif');
+            
+        }
+    else
+        {
+            arr[0].pause();
+            $('#icon_toggle').removeClass('fa-pause');
+            $('#icon_toggle').addClass('fa-play');
+            $('body').removeClass('add_gif');
+        }
+});
+$('body').on('keypress',function(event){
+    if(event.keyCode == 32)
+        {
+            console.log("on key press");
+            arr = $('.song_play');
+            if(arr[0].paused == true)
+                {
+                    console.log(" paused");
+                    arr[0].play();
+                    $('#icon_toggle').removeClass('fa-play');
+                    $('#icon_toggle').addClass('fa-pause');
+                    $('body').addClass('add_gif');
+
+                }
+            else
+                {
+                    console.log("play");
+                    arr[0].pause();
+                    $('#icon_toggle').removeClass('fa-pause');
+                    $('#icon_toggle').addClass('fa-play');
+                    $('body').removeClass('add_gif');
+                }
+        }
+});
