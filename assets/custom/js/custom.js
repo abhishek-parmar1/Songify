4103b78da5f4660ea1b4f27ee2cdb0e1b6eac405
Abhishek <parmarabhishek.53@gmail.com>
2017-07-11 01:09:10 +0530

    playlist page layout and button completed

--- assets/custom/js/custom.js
+++ assets/custom/js/custom.js
@@ -1,12 +1,58 @@
 // function to direct the user from the home screen section to the song playlist section
 $('.welcome-screen button').on('click', function() {
     var name = $('#name-input').val();
-    var message = "Welcome, " + name;
-    $('.main .user-name').text(message);
-    $('.welcome-screen').addClass('hidden');
-    $('.main').removeClass('hidden');
+    if(name.length > 0)
+        {
+            var message = "Welcome, " + name;
+            $('.main .user-name').text(message);
+            $('.welcome-screen').addClass('hidden');
+            $('.main').removeClass('hidden');
+        }
+    else{
+        $('#name-input').addClass('error');
+    }
 });
 
+// perform changes when the song ends
+$('.song-play')[0].onended = function(){
+    $('#play-icon').removeClass('fa-pause');
+    $('#play-icon').addClass('fa-play');
+    $('.content').removeClass('add-gif');
+};
+
+// function to perform operation when play btn or space bar is pressed
+function operationoOnPlayBtn()
+{
+    arr = $('.song-play');
+    if(arr[0].paused)
+        {
+            arr[0].play();
+            $('#play-icon').removeClass('fa-play');
+            $('#play-icon').addClass('fa-pause');
+            $('.content').addClass('add-gif');
+            
+        }
+    else
+        {
+            arr[0].pause();
+            $('#play-icon').removeClass('fa-pause');
+            $('#play-icon').addClass('fa-play');
+            $('.content').removeClass('add-gif');
+        }
+}
+
+// play and pause song on click of the button
+$('#play-icon').click(function(){
+    operationoOnPlayBtn()
+});
+
+// work for paying the song on click of spacebar
+$('body').on('keypress',function(event){
+    if(event.keyCode == 32)
+        {
+            operationoOnPlayBtn()
+        }
+});
 
 
 /*
@@ -14,48 +60,5 @@ $('.welcome-screen button').on('click', function() {
 $('#submit_song').click(function(){
     var song_name = $('#song_to_upload').val().replace(/C:\\fakepath\\/i, '');
     $("audio").attr("src","songs/" + song_name);
-});
-// work for paying the song on click of button
-$('.play_btn').click(function(){
-    arr = $('.song_play');
-    if(arr[0].paused == true)
-        {
-            arr[0].play();
-            $('#icon_toggle').removeClass('fa-play');
-            $('#icon_toggle').addClass('fa-pause');
-            $('body').addClass('add_gif');
-            
-        }
-    else
-        {
-            arr[0].pause();
-            $('#icon_toggle').removeClass('fa-pause');
-            $('#icon_toggle').addClass('fa-play');
-            $('body').removeClass('add_gif');
-        }
-});
-$('body').on('keypress',function(event){
-    if(event.keyCode == 32)
-        {
-            console.log("on key press");
-            arr = $('.song_play');
-            if(arr[0].paused == true)
-                {
-                    console.log(" paused");
-                    arr[0].play();
-                    $('#icon_toggle').removeClass('fa-play');
-                    $('#icon_toggle').addClass('fa-pause');
-                    $('body').addClass('add_gif');
-
-                }
-            else
-                {
-                    console.log("play");
-                    arr[0].pause();
-                    $('#icon_toggle').removeClass('fa-pause');
-                    $('#icon_toggle').addClass('fa-play');
-                    $('body').removeClass('add_gif');
-                }
-        }
 });
 */
