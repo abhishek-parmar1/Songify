d2b10868a25a92da2458d1ba732a5d5da8bf7cc6
Abhishek <parmarabhishek.53@gmail.com>
2017-07-14 02:34:07 +0530

    all the required objectives completed

--- assets/custom/js/custom.js
+++ assets/custom/js/custom.js
@@ -1,43 +1,43 @@
 // function to direct the user from the home screen section to the song playlist section
 $('.welcome-screen button').on('click', function() {
-    var name = $('#name-input').val();
-    var nameReg = /^\w+\s*$/;
-    if(name.length > 3 && nameReg.test(name))
+    var name = $('#name-input').val();             //take input value
+    var nameReg = /^\w+\s*$/;                      // regexp for name
+    if(name.length > 3 && nameReg.test(name))      // check name
         {
-            var message = "Welcome, " + name;
-            $('.main .user-name').text(message);
+            var message = "Welcome, " + name;   
+            $('.main .user-name').text(message);   //display user name 
             $('.welcome-screen').addClass('hidden');
             $('.main').removeClass('hidden');
         }
     else{
-        $('#name-input').addClass('error').val('Required 3+ letters');
+        $('#name-input').addClass('error').val("").attr('placeholder','Required 3+ letters'); //error
     }
 });
-
-// perform changes when the song ends
-$('.song-play')[0].onended = function(){
-    $('#play-icon').removeClass('fa-pause').addClass('fa-play');
-    $('.content').removeClass('add-gif');
-};
 
 // function to perform operation when play btn or space bar is pressed
 function toggleSong()
 {
-    arr = $('.song-play');
-    if(arr[0].paused)
+    arr = $('.song-play');    // get array of audio tags with class .song-play
+    if(arr[0].paused)         // if song paused
         {
             arr[0].play();
             $('#play-icon').removeClass('fa-play').addClass('fa-pause');
             $('.content').addClass('add-gif');
             
         }
-    else
+    else                      // if song is playing
         {
             arr[0].pause();
             $('#play-icon').removeClass('fa-pause').addClass('fa-play');
             $('.content').removeClass('add-gif');
         }
 }
+
+// perform changes when the song ends
+$('.song-play')[0].onended = function(){
+    $('#play-icon').removeClass('fa-pause').addClass('fa-play');
+    $('.content').removeClass('add-gif');
+};
 
 // play and pause song on click of the button
 $('#play-icon').click(function(){
@@ -46,7 +46,8 @@ $('#play-icon').click(function(){
 
 // work for paying the song on click of spacebar
 $('body').on('keypress',function(event){
-    if(event.keyCode == 32)
+    var target = event.target;
+    if(event.keyCode == 32  && target.tagName != 'INPUT' )
         {
             toggleSong()
         }
@@ -74,11 +75,11 @@ function fancyTimeFormat(time)
 
 // function to update the song duration and current time
 function updateCurrentTime(){
-    arr = $('.song-play');
+    arr = $('.song-play');            // get array of audio tags with class .song-play
     // ~~ for math.floor
-    time1 = ~~(arr[0].duration);
+    time1 = ~~(arr[0].duration);      // get song duration
     time1 = fancyTimeFormat(time1);
-    time2 = ~~(arr[0].currentTime);
+    time2 = ~~(arr[0].currentTime);   // get song current time 
     time2 = fancyTimeFormat(time2);
     $('.time-elapsed').text(time2);
     $('.song-duration').text(time1);
@@ -90,6 +91,7 @@ $('#play-icon').on('click',function(){
         updateCurrentTime(); 
     }, 1000);
 });
+
 // call the updateCurrentTime function continuously to update time on click of song
 $('.song').on('click',function(){
     setInterval(function(){
@@ -113,75 +115,98 @@ songObjectArray = [
         'songArtist' : 'Ed Sheeran',
         'songAlbum' : 'Album#1',
         'songDuration' : '4:23',
-        'songSrc' : 'songs/song1.mp3'
+        'songSrc' : 'song1.mp3',
+        'imagePath' : 'song1.jpg'
     },
     {
         'songName' : 'Despacito',
         'songArtist' : 'Luis Fonsi, Daddy Yankee',
         'songAlbum' : 'Album#2',
         'songDuration' : '4:41',
-        'songSrc' : 'songs/song2.mp3'
+        'songSrc' : 'song2.mp3',
+        'imagePath' : 'song2.jpg'
     },
     {
         'songName' : 'Something Just Like This',
         'songArtist' : 'The Chainsmokers',
         'songAlbum' : 'Album#3',
         'songDuration' : '4:23',
-        'songSrc' : 'songs/song3.mp3'
+        'songSrc' : 'song3.mp3',
+        'imagePath' : 'song3.jpg'
     },
     {
         'songName' : "That's What I Like",
         'songArtist' : 'Bruno Mars',
         'songAlbum' : 'Album#4',
         'songDuration' : '3:30',
-        'songSrc' : 'songs/song4.mp3'
+        'songSrc' : 'song4.mp3',
+        'imagePath' : 'song4.jpg'
     },
     {
         'songName' : 'Wigle Wigle',
         'songArtist' : 'Jason Derulo',
         'songAlbum' : 'Album#5',
         'songDuration' : '3:43',
-        'songSrc' : 'songs/song5.mp3'
+        'songSrc' : 'song5.mp3',
+        'imagePath' : 'song5.jpg'
     }
 ];
 
 //load all the songs details in the html
 window.onload = function(){
+    
+    // create the playlist section of songs
     for(var i=1;i<=songObjectArray.length;i++)
         {
-            $('#song' + i + " .name").text(songObjectArray[i-1].songName);
-            $('#song' + i + " .artist").text(songObjectArray[i-1].songArtist);
-            $('#song' + i + " .album").text(songObjectArray[i-1].songAlbum);
-            $('#song' + i + " .duration").text(songObjectArray[i-1].songDuration);
-        }    
+            var songSectionId = "#song" + i;              //get song id in html
+            var songSectionRef = $(songSectionId);        // ref to that id
+            var songObj = songObjectArray[i-1];           //get song position in array of objects   
+            songSectionRef.find('.name').text(songObj.songName);
+            songSectionRef.find('.artist').text(songObj.songArtist);
+            songSectionRef.find('.album').text(songObj.songAlbum);
+            songSectionRef.find('.duration').text(songObj.songDuration);
+            addSongSrcEventListener(songObj,i);          
+        }
+    
+    // for searching and sorting in the songs and creating its table
+    $('#song-playlist').DataTable({
+         paging:false
+     });
+    
+    // for the first song details
+    changeCurrentSongDetails(songObjectArray[0]);
+    
+    // set progress bar time fir first load to 0:0
     updateCurrentTime();
 }
 
 // function which creates the function that handles adding songs option 
-function addSongSrcEventListener(songSrc,position)
+function addSongSrcEventListener(currentSongObj,position)
 {
-    var songID = '#song' + position;
-    $(songID).on('click',function(){
-        var currentSong = $('audio');
-        var currentSongSrc = currentSong[0].src;
-        if(currentSongSrc.search(songSrc) != -1)
+    var songID = '#song' + position;                     // song id in html
+    $(songID).on('click',function(){                
+        var currentSong = $('.song-play');                
+        var currentSongSrc = currentSong[0].src;         //current song source
+        if(currentSongSrc.search(currentSongObj.songSrc) != -1)         // check with the provided source
             {
                 toggleSong();
             }
         else
         {
-            currentSong[0].src = songSrc;
-            $(".song").removeClass("active");
+            currentSong[0].src = "songs/" + currentSongObj.songSrc;
+            changeCurrentSongDetails(currentSongObj)
+            $(".song").removeClass("active");           
             $(this).addClass("active");
             toggleSong();
         }
     });
 }
 
-// call to create all the functions for adding songs
-for(var i = 0;i < songObjectArray.length;i++)
-{
-    addSongSrcEventListener(songObjectArray[i].songSrc,i+1);
+function changeCurrentSongDetails(currentSongObj){
+    var detailsSectionRef=  $(".song-details-section");
+    detailsSectionRef.find("#current-song-image").attr('src',"songs/song_images/" + currentSongObj.imagePath);
+    detailsSectionRef.find(".current-song-name").text(currentSongObj.songName);
+    detailsSectionRef.find(".current-song-artist").text(currentSongObj.songArtist);
 }
 
 //for the progress bar of the song
